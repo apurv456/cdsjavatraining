6b0f7ab944bc1f8c961753230cae4ff56a85becb
apurv456 <apurvverma456@gmail.com>
2024-07-19 16:16:29 +0530

    Done

---
+++ length.java
@@ -0,0 +1,12 @@
+public class length {
+    public static void main(String[] args) {
+        String name= "Apurv";
+        int length = 0;
+        for (char c: name.toCharArray()) {
+
+            length++;
+            
+        }
+        System.out.println(length);
+    }
+}
---
+++ swap.java
@@ -0,0 +1,10 @@
+class swap {
+    public static void main(String[] args) {
+     int a = 8;
+     int b = 3;
+    //   swap
+    int t = a;
+    a = b;
+    b = t;
+    }
+}
