21b345a2b58b5f16514c928cebe90eb834552fca
apurv456 <apurvverma456@gmail.com>
2024-07-22 16:04:05 +0530

    program using ternary operator

---
+++ TernaryOperator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class TernaryOperator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your age :");
+        int age = sc.nextInt();
+        String string = (age>=18)?"Eligible" :"Not eligible";
+        System.out.println(string);
+    
+        
+    }
+    
+}
